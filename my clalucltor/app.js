63e7b2e3e46058d12e6a7877d4e6232ada1355a9
MuhammadWaqar3325 <muhammadwaqar3325@gmail.com>
2021-11-19 16:46:27 +0000

    Update app.js

--- my clalucltor/app.js
+++ my clalucltor/app.js
@@ -4,3 +4,14 @@ function getHistory() {
 function printHistory(num) {
 	document.getElementById("history-val").innerText = num;
 }
+function getOutput() {
+	return document.getElementById("output-val").innerText;
+}
+function printOutput(num) {
+	if (num == "") {
+		document.getElementById("output-val").innerText = num;
+	}
+	else {
+		document.getElementById("output-val").innerText = getFormattedNumber(num);
+	}
+}
