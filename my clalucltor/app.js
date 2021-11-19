5f6facb9caec720a4f42e0c7ca1c9da42f9d9c6e
MuhammadWaqar3325 <muhammadwaqar3325@gmail.com>
2021-11-19 16:49:07 +0000

    Update app.js

--- my clalucltor/app.js
+++ my clalucltor/app.js
@@ -26,3 +26,36 @@ function getFormattedNumber(num) {
 function reverseNumberFormat(num) {
 	return Number(num.replace(/,/g, ''));
 }
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; i++) {
+	operator[i].addEventListener('click', function () {
+		if (this.id == "clear") {
+			printHistory("");
+			printOutput("");
+		}
+		
+		else {
+			var output = getOutput();
+			var history = getHistory();
+			if (output == "" && history != "") {
+				if (isNaN(history[history.length - 1])) {
+					history = history.substr(0, history.length - 1);
+				}
+			}
+			if (output != "" || history != "") {
+				output = output == "" ? output : reverseNumberFormat(output);
+				history = history + output;
+				if (this.id == "=") {
+					var result = eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else {
+					history = history + this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
+		}
+	});
+}
