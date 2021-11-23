ee0d70ca2f0942e3f26bdacf5f304b2e32ed2089
MuhammadWaqar3325 <muhammadwaqar3325@gmail.com>
2021-11-23 15:28:47 +0000

    Update app.js

--- my clalucltor/app.js
+++ my clalucltor/app.js
@@ -23,9 +23,7 @@ function getFormattedNumber(num) {
 	var value = n.toLocaleString("en");
 	return value;
 }
-function reverseNumberFormat(num) {
-	return Number(num.replace(/,/g, ''));
-}
+
 var operator = document.getElementsByClassName("operator");
 for (var i = 0; i < operator.length; i++) {
 	operator[i].addEventListener('click', function () {
