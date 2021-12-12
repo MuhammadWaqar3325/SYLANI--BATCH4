a20cbc1964026bfa76f582ad6f265bbf2e8797d2
Muhammad Waqar <65023745+MuhammadWaqar3325@users.noreply.github.com>
2021-12-12 09:32:52 +0500

    Update app.js

--- sylaniclasses/class#21/app.js
+++ sylaniclasses/class#21/app.js
@@ -1,3 +1,14 @@
-const double = () => 2;{
-const result = 2 * num ;
-return result};
+// const double = () => 2;{
+// const result = 2 * num ;
+// return result};
+
+
+// Tecnical diff
+
+const obj = {
+    name: "Sylani"
+    getName: function(){
+        return this.name;
+    }
+};
+console.log(obj.getName());
