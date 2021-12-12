8b6ef802dcc5414642900eb8aee6794e4a0f86bc
Muhammad Waqar <65023745+MuhammadWaqar3325@users.noreply.github.com>
2021-12-12 10:43:27 +0500

    app

--- sylaniclasses/class#21/app.js
+++ sylaniclasses/class#21/app.js
@@ -5,15 +5,16 @@
 
 // Tecnical diff
 
-const obj = {
-    name: "Sylani",
-    getName: function(){
-        return this.name;
-    }
-};
-console.log(obj.getName());
+// const obj = {
+//     name: "Sylani",
+//     getName: function(){
+//         return this.name;
+//     }
+// };
+// console.log(obj.getName());
 
 
 // alert ("hello");
 const names = prompt ("Enter your name");
-document.write(names)
+// document.write(names)
+
