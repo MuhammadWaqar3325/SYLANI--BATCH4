a5c49bc5a3da4fb5602f4635731a99db9f991b66
MuhammadWaqar3325 <65023745+MuhammadWaqar3325@users.noreply.github.com>
2021-12-15 09:59:51 -0800

    project 1

---
+++ freecode/project1/hex/app.js
@@ -0,0 +1,24 @@
+// (function (){
+//     const butt = document.getElementById("btn")
+//     const body = document.querySelector("body")
+//     const hexValue = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E"];
+   
+//     const value = document.querySelector("#hex-value")
+ 
+ 
+//     butt.addEventListener("click", changeColors)
+ 
+//      function changeColors(){
+//          let hex = "#";
+//          for (let i = 0; i < 6; i++) {
+//              const index = Math.floor(Math.random() * hexValue.length) ;
+//             hex += hexValue[index];
+ 
+             
+             
+//          }
+//          value.textContent= hex
+//          body.style.backgroundColor = hex;
+         
+//      }
+//  })();
