88bfecc973f80556472f3d4c7d877758f8cd4f3f
MuhammadWaqar3325 <65023745+MuhammadWaqar3325@users.noreply.github.com>
2022-02-16 22:57:32 +0500

    ttt

---
+++ freecode/project#1/hex.js
@@ -0,0 +1,20 @@
+// const butn = document.getElementById("btn");
+// // const colortext = document.getElementById("hex-value");
+// const body = document.querySelector("body")
+// const value = document.querySelector("#hex-value")
+// const hexValue = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+// // console.log(hexValue)
+
+// function getRandomNumber() {
+//     return Math.floor(Math.random() * hexValue.length)
+// }
+// // console.log(getRandomNumber())
+// butn.addEventListener('click', () => {
+//     let hexacolor = '#';
+
+//     for( let i = 0; i < 9; i++){
+//         hexacolor += hexValue[getRandomNumber()]
+//     }
+//     document.body.style.backgroundColor = hexacolor;
+// }) 
